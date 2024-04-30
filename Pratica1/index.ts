ac0bca2f1b990d353015df308ca96d0c0ccd1e02
LuanBarbs <luan.barbosa@estudante.ufjf.br>
2024-04-30 09:29:11 -0300

    pratica2

--- Pratica1/index.ts
+++ Pratica1/index.ts
@@ -1,5 +1,6 @@
 export{};
 
+// Prática 1
 function eCommerce(cash:number, product:string):string {
     let value:number;
     if(product === "Celular") value = 1000;
@@ -16,3 +17,36 @@ function eCommerce(cash:number, product:string):string {
 console.log(eCommerce(1000, "Celular"));
 console.log(eCommerce(500, "Celular"));
 console.log(eCommerce(500, "Mouse"));
+
+// Prática 2
+interface iVehicle {
+    serialNumber:number;
+    name:string;
+    maxQAllowed:number;
+};
+class Vehicle implements iVehicle {
+    serialNumber: number;
+    name: string;
+    maxQAllowed: number;
+    constructor(serialNumber:number, name:string, maxQAllowed:number) {
+        this.serialNumber = serialNumber;
+        this.name = name;
+        this.maxQAllowed = maxQAllowed;
+    }
+};
+class Automobile extends Vehicle {
+    constructor(serialNumber:number, name:string, maxQAllowed:number) {
+       super(serialNumber, name, maxQAllowed);
+    }
+    openTrunk():void {
+        console.log("Porta Malas Aberto!");
+    }
+};
+class Aircraft extends Vehicle {
+    constructor(serialNumber:number, name:string, maxQAllowed:number) {
+        super(serialNumber, name, maxQAllowed);
+    }
+    contactCenter():void {
+        console.log("Central na linha!");
+    }
+}
